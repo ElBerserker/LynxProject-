4d55eeed7dc55c28be0de0e0f643ffd0a27a5b06
ElBerserker <freeenergy1975@gmail.com>
2022-02-10 19:04:06 -0600

    Se agregaron validaciones para los atributos de la tabla users, se valida la longitud de los atributos y se válida si las contraseñas coinciden.

--- Back/Python_WS/esquema.py
+++ Back/Python_WS/esquema.py
@@ -13,6 +13,35 @@ class UserBaseModel(BaseModel):
     password: str
     status: str
 
+    @validator('id_user')
+    def id_user_validation_lenth(cls, id_user):
+        if len(id_user) < 3 or len(id_user) > 30:
+            raise ValueError('La longitud debe encontarse entre los 3 y 30 caracteres')
+
+        return id_user
+
+    @validator('type_of_user')
+    def type_of_user_validation_lenth(cls, type_of_user):
+        if len(type_of_user) < 3 or len(type_of_user) > 30:
+            raise ValueError('La longitud debe encontarse entre los 3 y 30 caracteres')
+
+        return  type_of_user
+
+    @validator('name_of_user')
+    def name_of_user_validation_lenth(cls, name_of_user):
+        if len(name_of_user) < 3 or len(name_of_user) > 30:
+            raise ValueError('La longitud debe encontarse entre los 3 y 30 caracteres')
+
+        return  name_of_user
+
+    @validator('password', 'confirm_password')
+    def validate_password(cls, password, values, **kwargs):
+        if not 'password' == 'confirm_password':
+            raise ValueError('Las contrasenias no coinciden')
+
+        return  password
+
+
 
 class SurveysBaseModel(BaseModel):
     name_survey: str
